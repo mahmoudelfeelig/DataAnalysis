e00314d5784f5298437735bf408256f52de8bd7f
mahmoudelfeelig <mahmoudelfeelig@gmail.com>
2024-11-28 22:06:27 +0100

    made Milestone3.py

---
+++ Milestone3.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+from scipy import stats
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.metrics import accuracy_score, precision_score, recall_score, mean_squared_error
+import matplotlib.pyplot as plt
